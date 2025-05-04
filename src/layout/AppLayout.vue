cd35ae0bd6b5fcc089cebde9adecebacd6aabdd3
Viktor Petruk <damnnn261@gmail.com>
2025-05-04 12:49:58 +0300

    initial comit

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <section class="relative bg-gradient-to-r from-cyan-400 to-blue-500">
+    <!-- TopBar -->
+    <div
+      class="fixed h-[3rem] px-5 flex items-center bg-gradient-to-r from-cyan-400 to-blue-500 w-full border-b-1 border-blue-500 top-0 z-50"
+    >
+      <div class="text-[1rem] font-[500]">KOGPA</div>
+
+      <div>
+        <Button class="h-[1.5rem] w-[1.5rem] bg-white hover:bg-white/80">
+          <Icon icon="lucide:menu" />
+        </Button>
+
+      </div>
+    </div>
+
+    <!-- Sidebar -->
+    <div></div>
+
+    <!-- Main Content -->
+    <div class="w-full mt-[3rem] h-[calc(100vh-3rem)]">
+      Something
+      <router-view name="main" />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from "../layout/AppLayout.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: AppLayout,
+      children: [
+      ]
+    },
+  ],
+})
+
+export default router
